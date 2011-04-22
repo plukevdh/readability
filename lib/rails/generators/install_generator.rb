f43dfda11fc9bf427575d3581e728a4171f7f7c8
plukevdh <hungerandthirst@gmail.com>
2011-04-22 11:09:30 -0400

    add routes as part of the install

--- lib/rails/generators/install_generator.rb
+++ lib/rails/generators/install_generator.rb
@@ -2,10 +2,15 @@ module Readability
   module Generators
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
-      desc "Creates a Readability config file for you to modify."
+      desc "Creates a Readability config file for you to modify and adds necessary routes."
 
       def copy_config
         template "readability.yml", "config/readability.yml"
+      end
+
+      def copy_routes
+        route "match 'readability/callback', to: 'controller#action', as: 'readability_callback'"
+        route "match 'readability/logout', to: 'controller#action', as: 'readability_logout'"
       end
 
       def show_readme
